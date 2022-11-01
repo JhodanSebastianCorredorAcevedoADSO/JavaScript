414a2410f08097cd7de6aebea8b2e53ad29f0f81
JhodanSebastianCorredorAcevedoADSO <corredorjhodan@gmail.com>
2022-11-01 12:15:24 -0500

    Solucion Ejercicio 3

--- main.js
+++ main.js
@@ -1,21 +1,32 @@
 addEventListener("DOMContentLoaded", (e) => {
 
     let primera = document.querySelector("#myForm");
-
+    let resultados = document.querySelector("#resultados")
     myForm.addEventListener("submit", (e) => {
         e.preventDefault();
         let dataInput = Object.fromEntries(new FormData(e.target));
-        let horas = dataInput.horas
+        let numero = dataInput.numero
+        let suma = 0
+        resultados.innerHTML = ""
+        for (let i = 1; i < numero; i++) {
+            if (numero % i == 0) {
+                suma += i
+            }
+        }
+        if (suma == numero) {
+            //alert(`${numero}Es perfecto`)
+            resultados.insertAdjacentHTML("beforeend", `
+                <h1>${numero} Es perfecto</h1>
+            `)
+        }
+        else {
+            //alert(`${numero} NO es perfecto`)
+            resultados.insertAdjacentHTML("beforeend", `
+                <h1>${numero} NO es perfecto</h1>
+            `)
+            
+        }
         
-        let resultados = document.querySelector("#tablaResul")
-        resultados.insertAdjacentHTML("beforeend",`
-        
-        <tr>
-            <td>${horas}</td>
-            <td>${horas*5208.33}</td>
-        </tr>
-        
-        `)
     })
-
+    
 })
