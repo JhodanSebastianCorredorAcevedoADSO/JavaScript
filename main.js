a2e4aa30d604630426d0f537b695ab0bf393ba1c
JhodanSebastianCorredorAcevedoADSO <corredorjhodan@gmail.com>
2022-11-08 08:22:21 -0500

    Solucion Ejercicio 4

--- main.js
+++ main.js
@@ -1,32 +1,26 @@
 addEventListener("DOMContentLoaded", (e) => {
 
-    let primera = document.querySelector("#myForm");
-    let resultados = document.querySelector("#resultados")
-    myForm.addEventListener("submit", (e) => {
+    let primera = document.querySelector("#ventas");
+    let resultados = document.querySelector("#resultados");
+    let Venta1 = document.querySelector("#Venta1");
+    let Venta2 = document.querySelector("#Venta2");
+    let Venta3 = document.querySelector("#Venta3");
+    let suldoB = 1000000
+
+    ventas.addEventListener("submit", (e) => {
+
         e.preventDefault();
+
         let dataInput = Object.fromEntries(new FormData(e.target));
-        let numero = dataInput.numero
-        let suma = 0
-        resultados.innerHTML = ""
-        for (let i = 1; i < numero; i++) {
-            if (numero % i == 0) {
-                suma += i
-            }
-        }
-        if (suma == numero) {
-            //alert(`${numero}Es perfecto`)
-            resultados.insertAdjacentHTML("beforeend", `
-                <h1>${numero} Es perfecto</h1>
-            `)
-        }
-        else {
-            //alert(`${numero} NO es perfecto`)
-            resultados.insertAdjacentHTML("beforeend", `
-                <h1>${numero} NO es perfecto</h1>
-            `)
-            
-        }
         
+        let Comision1 = Number(dataInput.Venta1) * 0.10
+        let Comision2 = Number(dataInput.Venta2) * 0.10
+        let Comision3 = Number(dataInput.Venta3) * 0.10
+        
+        let Comision = Comision1 + Comision2 + Comision3
+        let total = suldoB + Comision
+        
+        resultados.innerHTML = total
     })
     
 })
